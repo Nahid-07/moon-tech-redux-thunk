3c20e0049d683be4e6bd90ba8b79a1e35bd9e806
Nahid-07 <nahidasif1998@gmail.com>
2023-01-30 12:50:47 +0600

    stock toggler added

--- src/redux/actions/filterActions.js
+++ src/redux/actions/filterActions.js
@@ -1,8 +1,13 @@
-import { TOOGLE_BRAND } from "../actionTypes/actionTypes"
+import { TOOGLE_BRAND, TOOGLE_STOCK } from "../actionTypes/actionTypes"
 
 export const toggleBrand = (brandName)=>{
     return{
       type : TOOGLE_BRAND,
       payload : brandName
+    }
+  }
+export const toggleStock = ()=>{
+    return{
+      type : TOOGLE_STOCK,
     }
   }
